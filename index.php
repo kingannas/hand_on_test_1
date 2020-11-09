d022dc671e01fbdb849de8d0f9f26282c4fd90fb
kingannas <36188764+kingannas@users.noreply.github.com>
2020-11-10 00:48:29 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+  $my_string = "Hello Guys, I am Raja Amirul Annas Bin Raja Idris, the Cloud Computer expert!. Please go to my GitHub accoun https://github.com/kingannas "; 
+  echo $my_string;
+?>
